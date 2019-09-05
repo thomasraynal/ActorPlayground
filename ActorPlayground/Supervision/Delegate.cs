5f4194e9dfe89ff3d96f3e86a304c16a28de5c7c
Thomas Raynal <thomas.raynal2@gmail.com>
2019-09-05 20:07:02 +0200

    tidy 6

--- ActorPlayground/Supervision/Delegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ActorPlayground
-{
-    public delegate SupervisorDirective Decider(PID pid, Exception reason);
-
-}
